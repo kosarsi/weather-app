a536702afa203fb7548ab29d3077c7d6fa8a0526
Siddharth Kosaraju <siddhukosaraju@gmail.com>
2025-07-08 00:31:49 -0500

    Removed console logs

--- src/getDayOfWeek.js
+++ src/getDayOfWeek.js
@@ -1,5 +1,4 @@
 export default function getDayOfWeek(day) {
-    console.log(day); 
     let date = day.datetime; 
     const [year, month, dayNum] = date.split("-").map(Number);
     const dateObj = new Date(year, month - 1, dayNum); 
